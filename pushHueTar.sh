b236183bce420c622e983ba746402a818654083d
AmithshaS <amithsha.s@flipkart.com>
2020-12-13 18:08:00 +0530

    Script to push to Flipkart repo 6

--- pushHueTar.sh
+++ pushHueTar.sh
@@ -2,7 +2,8 @@
 
 mkdir -p pushHueTar/deb/tmp/huePackage
 #find . -name "*" ! -name 'pushHueTar' -exec cp "{}" pushHueTar/deb/tmp/huePackage/  \;
-find * -mindepth 1 -name "*" ! -name 'pushHueTar' -exec cp -rf --parents \{\} pushHueTar/deb/tmp/huePackage/  \;
+#find * -mindepth 1 -name "*" ! -name 'pushHueTar' -exec cp -rf --parents \{\} pushHueTar/deb/tmp/huePackage/  \;
+find * -mindepth 1 -name "*" ! -not -path 'pushHueTar'  -exec cp -rf --parents \{\} pushHueTar/deb/tmp/huePackage/  \;
 ls pushHueTar
 
 cd pushHueTar
